cea69ba06b784ec8976df5588f77c69187e4a096
AbhaySinghNaruka <abhaynaruka.asn@gmail.com>
2022-10-25 15:19:46 +0200

    small changes on js

--- mail/js/script.js
+++ mail/js/script.js
@@ -5,5 +5,5 @@ const allowedEmail = ['abhaysingh.naruka@gmail.com', 'name.surname@gmail.com'];
 if (qEmail == allowedEmail) {
     console.log(qEmail[allowedEmail]);
 } else {
-    console.log('Non hai il permesso per avere una bella vita')
+    console.log('Non hai il permesso')
 }
